1681e365a87008b877243d8169f46ce2e06471ff
Geiger <gkrizsi07@gmail.com>
2025-09-23 12:24:34 +0200

    kezdet2

--- elso.py
+++ elso.py
@@ -10,6 +10,7 @@ for i in range(100):
 
     #szám elhelyezése a listába
     szamok.append(rszam)
+    
 #ellenőrzés
 print(szamok)
 
@@ -21,4 +22,23 @@ jatek_szam = 0
 nem_talaltdb = 0
 
 #kitalálandó szám beállítása
-kitalalando_szam = input()
+kitalalando_szam = szamok[random.randint(0, len(szamok))]
+
+tipp = int(input("Kérek egy egész számot [1-100]: "))
+
+if (tipp == kitalalando_szam):
+    print("De jó neked")
+elif(tipp < kitalalando_szam):
+    print("A szám nagyob!")
+else:
+    print("A szám kicib!")
+    
+while(tipp != kitalalando_szam):
+    tipp = int(input("Kérek egy egész számot [1-100]: "))
+    
+    if (tipp == kitalalando_szam):
+        print("De jó neked")
+    elif(tipp < kitalalando_szam):
+        print("A szám nagyob!")
+    else:
+        print("A szám kicib!")
