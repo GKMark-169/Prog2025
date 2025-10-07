0c4328343757906df11e63bafd0b3099b5bec63d
Geiger <gkrizsi07@gmail.com>
2025-10-07 12:24:22 +0200

    valami

--- fajlTeszt.py
+++ fajlTeszt.py
@@ -1,19 +1,47 @@
+
+verseny_adatok=[]
+
 try:
     with open("F1-2024dec.csv", encoding="utf-8") as fajl:
-        f= fajl.read()
-        n=4/0
-    
-        print(f)
-        print(n)
-        
-        
-        
-        
+        verseny_adatok= fajl.readlines()
 
+        
+except Exception as ex:
+    print("Cs! : Hiba oka:{ex}")
 except FileExistsError: 
     print("Gebasz van baszod!!!!")
-except ZeroDivisionError:
-    print("Ne osz 0-al!!!!")
-except Exception:
-    print("Cs! : Hiba oka:{ex}")
+
+'''
+   1. [Megszámolás] 
+   2. [Eldöntés 1]
+      [Eldöntés 2]
+   3. [Kiválasztás]
+   4. [Keresés]
+   5. [Sorozatszámítás, összegzés]
+   6. [Minimum/Maximum kiválasztás]
+   
+   7. [Másolás]
+   8. [Kiválogatás]
+   9. [Szétválogatás]
+   10.[Metszet]
+   11.[Únió]
+   
+   12.Rendezés
+      [Egyszerű cserés rendezés]
+      [Buborékos]
+      [Minimum kiválasztásos]
+'''
+#------------------------------------------------------------------------------
+# 1. Hány versenyző nem szerzett még pontot?
+db=0
+for i in range(1, len(verseny_adatok)):
+    if (int(verseny_adatok[i].split(",")[1])==0):
+        db=db+1
+        
+        
+    
+print(f"{db} versenyző nem szerzett még pontott. \n")
+
+#------------------------------------------------------------------------------------
+#2. 
 print("Haaamooonddd!!")
