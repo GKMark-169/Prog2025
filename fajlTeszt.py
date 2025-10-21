781ab56ee48c98cbaddb8786981cc4594b8a652c
Geiger <gkrizsi07@gmail.com>
2025-10-21 12:23:45 +0200

    igen

--- fajlTeszt.py
+++ fajlTeszt.py
@@ -12,8 +12,8 @@ except FileExistsError:
     print("Gebasz van baszod!!!!")
 
 '''
-   1. [Megszámolás] 
-   2. [Eldöntés 1]
+   1. [Megszámolás] [X]
+   2. [Eldöntés 1] [X]
       [Eldöntés 2]
    3. [Kiválasztás]
    4. [Keresés]
@@ -43,5 +43,39 @@ for i in range(1, len(verseny_adatok)):
 print(f"{db} versenyző nem szerzett még pontott. \n")
 
 #------------------------------------------------------------------------------------
-#2. 
-print("Haaamooonddd!!")
+#2. Van-e Fernando nevű versenyző?
+i = 0
+while(i<len(verseny_adatok) and "Fernando" not in verseny_adatok[i]):
+    i+=1
+if (i<len(verseny_adatok)):
+    print("Van Fernando versenyző!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+else:
+    print("Nincs ilyen he!!!!")
+
+# 2.B Mindenki szerzett-e már 90 pontott?????????????
+i=1
+while (i<len(verseny_adatok) and int(verseny_adatok[i].split(",")[1]))>90:
+    i=+1
+if i==len(verseny_adatok):
+    print("igen")
+else:
+    print("nem")
+    
+#3. Melyik istálló pilotája Yuki Tsunoda?
+i=0
+while verseny_adatok[i].split(",")[0]!="Yuki Tsunoda":
+    i+=1
+print(f"Yuki Tsunoda helye: {verseny_adatok[i].split(",")[2]}")
+
+#4.Melyik csapatban volt Pierre Gasly?
+i=1
+while i<len(verseny_adatok) and "Pierre Gasly" not in verseny_adatok[i]:
+    i+=1
+if i<len(verseny_adatok):
+    print("Pierre Gasly", verseny_adatok[i].split(",")[2].strip()),"csapatba van!   :)")
+else:
+    print("Pierre Gasly nics!")
+    
+
+print("DOOOOOOOOMMMMMAAAAAAAA!!")
+
